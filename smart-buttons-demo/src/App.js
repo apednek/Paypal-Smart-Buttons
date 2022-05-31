e6946449253fc8d7e7b434086a3665f6bd297e5b
Aditi Shivaji Pednekar <adi410pednekar@gmail.com>
2022-05-30 21:15:25 -0700

    Initial commit

---
+++ smart-buttons-demo/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom"
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+function App() {
+  const [price, setPrice] = useState(0)
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: price,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
+
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <input type = "number" onChange = {e => setPrice(e.target.value)} value = {price}/>
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
